d79f52a75fa0d5bf17345d686fadb78c31e107d2
Aaron Graves <aaron.graves@gmail.com>
2012-06-05 20:20:30 -0400

    Initial rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = '750074814ba1211eeef53ec650dd9bf05da8d8346fbe6b186019cf6132a997b2aeb97a87b38ded27f52a49102f5d2329b89acc63e9363e4fc1278a4f0b1735a4'
--- db/migrate/20120606001601_create_items_table.rb
+++ db/migrate/20120606001601_create_items_table.rb
@@ -1,7 +1,13 @@
 class CreateItemsTable < ActiveRecord::Migration
   def up
+    create_table :items do |t|
+      t.string :name
+      t.decimal :price
+      t.integer :supplier_id
+    end
   end
 
   def down
+    drop_table :items
   end
 end
